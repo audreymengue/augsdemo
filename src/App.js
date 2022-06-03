e70576003d9c97e2c8595cd7296cfa268119a286
Audrey Mengue <massoumeharmonie@gmail.com>
2022-06-03 23:14:19 +0000

    Updated yml file

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ function App() {
   return (
     <main>
       <section className='container'>
-        <h3>Hey, CI/CD here with Azure devOps pipelines, we tried and it worked perfectly.</h3>
+        <h3>Hey, CI/CD here with Azure devOps pipelines, we tried and it worked perfectly today too.</h3>
        
       </section>
     </main>

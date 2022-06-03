361a8785029eb4f1a1caa2fc07ce554792ae9f1b
Audrey Mengue <massoumeharmonie@gmail.com>
2022-06-03 23:22:45 +0000

    Added a new section

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ function App() {
         <h3>Hey, CI/CD Demo 1 was a success</h3>
        
       </section>
+      <section className='container'>
+        <h3>Hey, CI/CD here with Azure devOps pipelines, we tried and it worked perfectly today too.</h3>
+        <h3>Hey, CI/CD Demo 1 was a success</h3>
+       
+      </section>
     </main>
   )
 }
